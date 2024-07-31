f6390768c8127495d8059d70b7d68a38ba6687b5
nlvvch <5011896+nlvvch@users.noreply.github.com>
2024-07-31 18:14:19 +0530

    changed text

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: `./index.html
+  `,
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
